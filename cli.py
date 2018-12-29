8d14d50041a13d766afd87dace550bdb315c4d5e
Mehdi Cherti <mehdicherti@gmail.com>
2018-12-29 21:56:55 +0100

    first commit

---
+++ cli.py
@@ -0,0 +1,73 @@
+import matplotlib as mpl
+mpl.use('Agg')
+from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import torch
+from torch import optim
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+from model import VAE, VAE_CPPN
+from transforms import Scale, Compose, PadTrim
+from data import Dataset
+from clize import run
+def train(
+        *,
+        data_folder='data',
+        nb=None,
+        lr=1e-3, 
+        batch_size=32, 
+        epochs=1000,
+        input_dim=1,
+        max_len=500,
+        log_interval=50,
+        latent_size=10,
+        cuda=False):
+    vae = VAE_CPPN(latent_size=latent_size, output_dim=max_len)
+    if cuda:
+        vae = vae.cuda()
+    optimizer = optim.Adam(
+        vae.parameters(), lr=lr,
+    )
+    vae.train()
+    epoch_start = 1
+    transform = Compose([
+        PadTrim(max_len=max_len),
+        Scale(),
+    ])
+    if nb:
+        nb = int(nb)
+    dataset = Dataset(data_folder, transform=transform, nb=nb)
+    print(len(dataset))
+    dataloader = DataLoader(dataset, batch_size=batch_size)
+    nb_iter = 0
+    for epoch in range(epoch_start, epochs+1):
+        for batch_index, data in enumerate(dataloader):
+            x = data
+            x = x.cuda() if cuda else x
+            vae.zero_grad()
+            xrec, mu, logvar = vae(x)
+            loss = vae.loss_function(x, xrec, mu, logvar)
+            loss.backward()
+            optimizer.step()
+            if nb_iter % log_interval == 0:
+                print(f'niter: {nb_iter:05d} loss: {loss.item():.4f}')
+                x = x.detach().cpu().numpy()
+                xrec = xrec.detach().cpu().numpy()
+                signal = x[0:3, 0].T
+                fake_signal = xrec[0:3, 0].T
+                for i in range(len(xrec)):
+                    s = xrec[i, 0]
+                    wavfile.write(f'out/fake_{i:03d}.wav', 16000, s)
+                for i in range(len(x)):
+                    s = x[i, 0]
+                    wavfile.write(f'out/real_{i:03d}.wav', 16000, s)
+                fig = plt.figure(figsize=(50, 10))
+                plt.plot(signal, color='blue', label='true')
+                plt.plot(fake_signal, color='orange', label='fake')
+                #plt.legend()
+                plt.savefig('out.png')
+                plt.close(fig)
+            nb_iter += 1
+
+if __name__ == '__main__':
+    run(train)
---
+++ model.py
@@ -0,0 +1,123 @@
+from clize import run
+from torch.nn import init
+from torch.nn.init import xavier_uniform
+import torch
+import time
+import os
+import pickle
+import numpy as np
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import grad
+from data import Dataset
+
+class Sin(nn.Module):
+
+    def forward(self, x):
+        return torch.sin(x)
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0)
+    elif classname == 'Linear':
+        xavier_uniform(m.weight.data)
+        m.bias.data.fill_(0)
+
+class VAE_CPPN(nn.Module):
+    def __init__(self, latent_size=100, output_dim=10):
+        super().__init__()
+        self.latent_size = latent_size
+        self.encode = nn.Sequential(
+            nn.Conv1d(1, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv1d(64, 128, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv1d(128, latent_size * 2, 4, 2, 1),
+            nn.ReLU(True),
+            nn.AdaptiveAvgPool1d(1),
+        )
+        self.decode = nn.Sequential(
+            nn.Linear(latent_size * 2, 256),
+            nn.Tanh(),
+            nn.Linear(256, 128),
+            nn.Tanh(),
+            nn.Linear(128, 1),
+            nn.Tanh(),
+        )
+        self.apply(weights_init)
+
+    def forward(self, x):
+        h = self.encode(x)
+        h = h.view(h.size(0), h.size(1))
+        mu, logvar = h[:, 0:self.latent_size], h[:, self.latent_size:]
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        h = mu + eps * std
+        z = torch.zeros(h.size(0), x.size(2), self.latent_size * 2)
+        z[:, :, 0:self.latent_size] = h.view(h.size(0), 1, h.size(1))
+        t = torch.linspace(-1, 1, x.size(2))
+        t = t.view(1, -1, 1)
+        z[:, :, self.latent_size:] = t
+        z_ = z.view(-1, self.latent_size * 2) 
+        xrec = self.decode(z_)
+        xrec = xrec.view(z.size(0), 1, z.size(1))
+        return xrec, mu, logvar
+
+    def loss_function(self, x, xrec, mu, logvar):
+        x = x.view(x.size(0), -1)
+        xrec = xrec.view(xrec.size(0), -1)
+        mse = ((xrec - x) ** 2).sum(1).mean()
+        kld = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp()).sum(1).mean()
+        return mse + kld
+
+
+
+class VAE(nn.Module):
+    def __init__(self, latent_size=100, output_dim=10):
+        super().__init__()
+        self.latent_size = latent_size
+        self.encode = nn.Sequential(
+            nn.Conv1d(1, 64, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv1d(64, 128, 4, 2, 1),
+            nn.ReLU(True),
+            nn.Conv1d(128, latent_size * 2, 4, 2, 1),
+            nn.ReLU(True),
+            nn.AdaptiveAvgPool1d(1),
+        )
+        self.decode = nn.Sequential(
+            nn.Linear(latent_size, 256),
+            nn.Tanh(),
+            nn.Linear(256, 512),
+            nn.Tanh(),
+            nn.Linear(512, output_dim),
+            nn.Tanh(),
+        )
+        self.apply(weights_init)
+
+    def forward(self, x):
+        h = self.encode(x)
+        h = h.view(h.size(0), h.size(1))
+        mu, logvar = h[:, 0:self.latent_size], h[:, self.latent_size:]
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        h = mu + eps * std
+        xrec = self.decode(h)
+        xrec = xrec.view(x.size())
+        return xrec, mu, logvar
+
+    def loss_function(self, x, xrec, mu, logvar):
+        x = x.view(x.size(0), -1)
+        xrec = xrec.view(xrec.size(0), -1)
+        mse = ((xrec - x) ** 2).sum(1).mean()
+        kld = -0.5 * (1 + logvar - mu.pow(2) - logvar.exp()).sum(1).mean()
+        return mse + kld
+if __name__ == '__main__':
+    ae = VAE()
+    x = torch.randn(32, 1, 16000)
+    xrec, mu, logvar = ae(x)
+    print(xrec.size(), mu.size(), logvar.size())
